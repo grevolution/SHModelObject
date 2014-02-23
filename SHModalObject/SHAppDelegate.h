0f01d89f1529d5c5b5f9d48024c3c3144d5e23ff
Shan Ul Haq <g@grevolution.me>
2014-02-23 19:45:59 +0800

    Initial Commit

---
+++ SHModalObject/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  SHModalObject
+//
+//  Created by SHAN UL HAQ on 23/2/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SHModalObject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SHModalObject
+//
+//  Created by SHAN UL HAQ on 23/2/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+    }
+}
